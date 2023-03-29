d82f04bffc8dd2be3dbacbfa92b8b498bd2eb33f
Sibiharan <124172962+sibiharanr@users.noreply.github.com>
2023-03-29 10:21:01 +0530

    Update App.js

--- React-Day-3/1.ClassWork/App.js
+++ React-Day-3/1.ClassWork/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import Appbar from './Day 3/Appbar';
-import Image from './Day 3/Image';
+import MenuItem from './MenuItem';
+
+
 
 function App() {
   return (
-    <div >
-      <Appbar/>
-      <br></br>
-      <Image/>
+    <div className="App">
+      
+ <MenuItem/>
     </div>
   );
 }
